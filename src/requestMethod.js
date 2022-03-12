ba8115ea690835aa3e6b741b8f85c0d476c60942
sourabh <51184750+sourabhpatole@users.noreply.github.com>
2022-03-12 22:19:08 +0530

    fetch accessToken

--- src/requestMethod.js
+++ src/requestMethod.js
@@ -1,8 +1,13 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
-const TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMmFmMjljZWIyMmRiNjJiMmNmMmQ5ZCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0NzAyNDQ3MywiZXhwIjoxNjQ3MjgzNjczfQ.t9fpeEYaWNAurTLCpKeqKWf64jDcztp4RDvMs4Nh0mM";
+const TOKEN = "";
+
+console.log(
+  JSON.parse(JSON.parse(localStorage.getItem("persist:root")).currentUser)
+    .accessToken
+);
+
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
 });
